901f8ca6a26d55875e329d5b327fded01b4696e0
Thakursaab002 <sushant1704.becse24@chitkara.edu.in>
2025-02-08 21:42:46 +0530

    Solution to Check Opposite Signs

--- Check Opposite Signs@@67882a731b4392d8fc1577e1/code.c
+++ Check Opposite Signs@@67882a731b4392d8fc1577e1/code.c
@@ -1,16 +1,10 @@
 #include <stdio.h>
-#include <stdbool.h>
-
-bool haveOppositeSigns(int a, int b) {
-    return ((a ^ b) < 0);
-}
 
 int main() {
     int a, b;
-    printf("Enter two space-separated integers: ");
     scanf("%d %d", &a, &b);
-    
-    if (haveOppositeSigns(a, b)) {
+
+    if ((a < 0 && b > 0) || (a > 0 && b < 0)) {
         printf("True\n");
     } else {
         printf("False\n");
