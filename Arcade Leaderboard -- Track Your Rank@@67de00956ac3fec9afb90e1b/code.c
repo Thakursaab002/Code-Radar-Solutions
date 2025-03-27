37889764aa8b3592d605578c107cd863d0c45d2e
SUSHANT THAKUR <sushant1704.becse24@chitkara.edu.in>
2025-03-27 18:54:05 +0530

    Solution to Arcade Leaderboard -- Track Your Rank

--- Arcade Leaderboard -- Track Your Rank@@67de00956ac3fec9afb90e1b/code.c
+++ Arcade Leaderboard -- Track Your Rank@@67de00956ac3fec9afb90e1b/code.c
@@ -5,14 +5,14 @@ void trackPlayerRanks(int ranked[], int player[], int n, int m, int result[]);
 int main() {
     int n, m;
     scanf("%d", &n);
-    int ranked[200000];
+    int ranked[n];
     for (int i = 0; i < n; i++) scanf("%d", &ranked[i]);
 
     scanf("%d", &m);
-    int player[200000];
+    int player[m];
     for (int i = 0; i < m; i++) scanf("%d", &player[i]);
 
-    int result[200000];
+    int result[m];
     trackPlayerRanks(ranked, player, n, m, result);
 
     for (int i = 0; i < m; i++) printf("%d\n", result[i]);
@@ -21,18 +21,21 @@ int main() {
 }
 
 void trackPlayerRanks(int ranked[], int player[], int n, int m, int result[]) {
-    int denseRanks[200000], rank = 1;
-    denseRanks[0] = rank;
+    int uniqueRanks[n], rank = 1, index = 0;
+    uniqueRanks[0] = ranked[0];
 
     for (int i = 1; i < n; i++) {
-        if (ranked[i] != ranked[i - 1]) rank++;
-        denseRanks[i] = rank;
+        if (ranked[i] != ranked[i - 1]) {
+            uniqueRanks[++index] = ranked[i];
+        }
     }
 
-    int i = n - 1, j = 0;
+    int uniqueCount = index + 1;
+    int i = uniqueCount - 1, j = 0;
+
     while (j < m) {
-        while (i >= 0 && player[j] >= ranked[i]) i--;
-        result[j] = i == -1 ? 1 : denseRanks[i] + 1;
+        while (i >= 0 && player[j] >= uniqueRanks[i]) i--;
+        result[j] = i + 2;
         j++;
     }
 }
