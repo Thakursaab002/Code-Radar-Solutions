23050de983f2604fe30f19e8e49892496cc2ad38
SUSHANT THAKUR <sushant1704.becse24@chitkara.edu.in>
2025-03-27 19:04:37 +0530

    Solution to Arcade Leaderboard -- Track Your Rank

--- Arcade Leaderboard -- Track Your Rank@@67de00956ac3fec9afb90e1b/code.c
+++ Arcade Leaderboard -- Track Your Rank@@67de00956ac3fec9afb90e1b/code.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-void trackPlayerRanks(int ranked[], int player[], int n, int m, int result[]) {
-    int uniqueRanks[n], rank = 1, index = 0;
+void trackPlayerRanks(int* ranked, int* player, int n, int m, int* result) {
+    int* uniqueRanks = (int*)malloc(n * sizeof(int));
+    int rank = 1, index = 0;
     uniqueRanks[0] = ranked[0];
 
     for (int i = 1; i < n; i++) {
@@ -18,22 +20,27 @@ void trackPlayerRanks(int ranked[], int player[], int n, int m, int result[]) {
         result[j] = i + 2;
         j++;
     }
+
+    free(uniqueRanks);
 }
 
 int main() {
     int n, m;
     scanf("%d", &n);
-    int ranked[n];
+    int* ranked = (int*)malloc(n * sizeof(int));
     for (int i = 0; i < n; i++) scanf("%d", &ranked[i]);
 
     scanf("%d", &m);
-    int player[m];
+    int* player = (int*)malloc(m * sizeof(int));
     for (int i = 0; i < m; i++) scanf("%d", &player[i]);
 
-    int result[m];
+    int* result = (int*)malloc(m * sizeof(int));
     trackPlayerRanks(ranked, player, n, m, result);
 
     for (int i = 0; i < m; i++) printf("%d\n", result[i]);
 
+    free(ranked);
+    free(player);
+    free(result);
     return 0;
 }
