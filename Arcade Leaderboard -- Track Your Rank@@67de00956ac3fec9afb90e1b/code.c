356118bc0388dd977e70f9aa449a47b0757bd7b9
SUSHANT THAKUR <sushant1704.becse24@chitkara.edu.in>
2025-03-27 18:55:40 +0530

    Solution to Arcade Leaderboard -- Track Your Rank

--- Arcade Leaderboard -- Track Your Rank@@67de00956ac3fec9afb90e1b/code.c
+++ Arcade Leaderboard -- Track Your Rank@@67de00956ac3fec9afb90e1b/code.c
@@ -1,7 +1,24 @@
 #include <stdio.h>
+
 void trackPlayerRanks(int ranked[], int player[], int n, int m, int result[]) {
     int uniqueRanks[n], rank = 1, index = 0;
     uniqueRanks[0] = ranked[0];
+
+    for (int i = 1; i < n; i++) {
+        if (ranked[i] != ranked[i - 1]) {
+            uniqueRanks[++index] = ranked[i];
+        }
+    }
+
+    int uniqueCount = index + 1;
+    int i = uniqueCount - 1, j = 0;
+
+    while (j < m) {
+        while (i >= 0 && player[j] >= uniqueRanks[i]) i--;
+        result[j] = i + 2;
+        j++;
+    }
+}
 
 int main() {
     int n, m;
@@ -19,21 +36,4 @@ int main() {
     for (int i = 0; i < m; i++) printf("%d\n", result[i]);
 
     return 0;
-}
-
-
-    for (int i = 1; i < n; i++) {
-        if (ranked[i] != ranked[i - 1]) {
-            uniqueRanks[++index] = ranked[i];
-        }
-    }
-
-    int uniqueCount = index + 1;
-    int i = uniqueCount - 1, j = 0;
-
-    while (j < m) {
-        while (i >= 0 && player[j] >= uniqueRanks[i]) i--;
-        result[j] = i + 2;
-        j++;
-    }
 }
