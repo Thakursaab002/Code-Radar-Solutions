e3698f6b39283aa9414ac3ca9446d935e88d73a7
SUSHANT THAKUR <sushant1704.becse24@chitkara.edu.in>
2025-04-01 01:59:27 +0530

    Solution to Fibonacci Series

--- Fibonacci Series@@67ca7acd1fe08d7207c253c6/code.c
+++ Fibonacci Series@@67ca7acd1fe08d7207c253c6/code.c
@@ -2,8 +2,8 @@
 int fibonacciseries(int n){
     if(n<=1){
         return n;
-        else{
-            return fibonacciseries(n-1)+fibonacciseries(n-2);
+    else{
+        return fibonacciseries(n-1)+fibonacciseries(n-2);
         }
     }
 }
